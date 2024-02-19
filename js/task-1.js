f9b18868f673ec985eb1f4fa89cdf94e50175d58
Eugene1380 <eugene.volobuyev@gmail.com>
2024-02-19 13:26:19 +0200

    hw-3

--- js/task-1.js
+++ js/task-1.js
@@ -1,15 +1,41 @@
-function makeTransaction(quantity, pricePerDroid, customerCredits) {
-  const totalPrice = quantity * pricePerDroid;
+// Задача 1. Генератор slug
 
-  if (customerCredits < totalPrice) {
-    return `Insufficient funds!`;
-  } else {
-    return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
-  }
+// Термін slug — це зрозумілий людині унікальний ідентифікатор,
+// який використовується у веб розробці для створення читабельних URL - адрес.
+// Наприклад, замість того, щоб користувач побачив в адресному рядку mysite.com / posts / 1q8fh74tx,
+// можна зробити slug із назви статті.У результаті адреса буде приємнішою для сприйняття: mysite.com / posts / arrays -for-begginers.
+
+// Slug — це завжди рядок у нижньому регістрі, слова якого розділені тире.
+
+// Напиши функцію slugify(title), яка приймає заголовок статті, параметр title і повертає slug, створений із цього рядка.
+
+// Значенням параметра title будуть рядки, слова яких розділені лише пробілами.
+// Усі символи slug повинні бути в нижньому регістрі.
+// Усі слова slug повинні бути розділені тире.
+// Візьми код нижче і встав після оголошення своєї функції для перевірки коректності її роботи. У консоль будуть виведені результати її роботи.
+
+// console.log(slugify("Arrays for begginers")); // "arrays-for-begginers"
+// console.log(slugify("English for developer")); // "english-for-developer"
+// console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
+// console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
+
+// Залиш цей код для перевірки ментором.
+
+// На що буде звертати увагу ментор при перевірці:
+
+// Оголошена функція slugify(title)
+// Виклик slugify("Arrays for begginers") повертає "arrays-for-begginers"
+// Виклик slugify("English for developer") повертає "english-for-developer"
+// Виклик slugify("Ten secrets of JavaScript") повертає "ten-secrets-of-javascript"
+// Виклик slugify("How to become a JUNIOR developer in TWO WEEKS") повертає "how-to-become-a-junior-developer-in-two-weeks"
+
+'use strict';
+
+function slugify(title) {
+  return title.toLowerCase().split(' ').join('-');
 }
 
-console.log(makeTransaction(5, 3000, 23000)); // "You ordered 5 droids worth 15000 credits!"
-console.log(makeTransaction(3, 1000, 15000)); // "You ordered 3 droids worth 3000 credits!"
-console.log(makeTransaction(10, 5000, 8000)); // "Insufficient funds!"
-console.log(makeTransaction(8, 2000, 10000)); // "Insufficient funds!"
-console.log(makeTransaction(10, 500, 5000)); // "You ordered 10 droids worth 5000 credits!"
+console.log(slugify('Arrays for begginers')); // "arrays-for-begginers"
+console.log(slugify('English for developer')); // "english-for-developer"
+console.log(slugify('Ten secrets of JavaScript')); // "ten-secrets-of-javascript"
+console.log(slugify('How to become a JUNIOR developer in TWO WEEKS')); // "how-to-become-a-junior-developer-in-two-weeks"
